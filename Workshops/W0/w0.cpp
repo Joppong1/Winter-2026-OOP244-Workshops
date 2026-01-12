9279b1076459d8174ebd02da47ccd2a51b583a61
Josh Oppong <joppong5@myseneca.ca>
2026-01-12 11:41:28 -0500

    Workshop 0 is ready to be submitted

---
+++ Workshops/W0/w0.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "#### Workshop Zero ####" << endl;
+	cout << "Subject and Section" << endl;
+	cout << "OOP244 NCC" << endl;
+	cout<< "Name:" << endl;
+	cout << "Joshua Oppong" << endl;
+	cout << "Student Number:" << endl;
+	cout << "154399201" << endl;
+	cout << "Email:" << endl;
+	cout << "joppong5@myseneca.ca" << endl;
+	return 0;
+}
